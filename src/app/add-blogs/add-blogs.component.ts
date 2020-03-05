82087620d43c424a065ffaecf5fb654481a8b88c
dfrank7 <francisoladipo5@gmail.com>
2020-03-05 15:01:47 +0100

    Adding addblogs method

--- src/app/add-blogs/add-blogs.component.ts
+++ src/app/add-blogs/add-blogs.component.ts
@@ -31,7 +31,8 @@ export class AddBlogsComponent implements OnInit {
     private cd: ChangeDetectorRef) {
     this.blogForm = this.formBuilder.group({
       title: ['', Validators.compose([Validators.required])],
-      details: ['', Validators.compose([Validators.required])]
+      details: ['', Validators.compose([Validators.required])],
+      image_url:['', Validators.compose([Validators.required])]
     });
   }
 
@@ -51,40 +52,39 @@ export class AddBlogsComponent implements OnInit {
     //   this.validateAllFormFields(this.blogForm);
     //   return;
     // }
-    // this.loader.showLoader();
-    // const path = Math.random().toString(36).substring(2);
-    // this.ref = this.afStorage.ref(path);
-    // this.task = this.ref.put(this.fileUpload);
-    // this.uploadProgress = this.task.percentageChanges();
+   // this.loader.showLoader();
+    const path = Math.random().toString(36).substring(2);
+    this.ref = this.afStorage.ref(path);
+    this.task = this.ref.put(this.fileUpload);
+    this.uploadProgress = this.task.percentageChanges();
 
-    // this.task.snapshotChanges().pipe(
-    //   finalize(() => {
-    //     this.ref.getDownloadURL().subscribe((url) => {
-    //       this.downloadURL = url
-          
-    //       this.service.addBlogs(payload).subscribe(user => {
-    //         this.router.navigateByUrl('/apps')
-    //       }, error => {
-    //         console.log('Error : ', error)
-    //         // this.loader.presentToast(error.error.error);
-    //         // this.loader.presentToast(error.error.message);
-    //       });
-    //       // location.reload()
-    //     })
-    //   })).subscribe()
+    this.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.ref.getDownloadURL().subscribe((url) => {
+          this.downloadURL = url
+          let payload = {
+            image_url: this.downloadURL,
+            title: this.blogForm.value.title,
+            details: this.blogForm.value.details
+          }
+          console.log('payload : ', payload);
+          this.service.addBlogs(payload).subscribe(user => {
+            this.router.navigateByUrl('/apps')
+            console.log('data saved')
+          }, error => {
+            console.log('Error : ', error)
+            // this.loader.presentToast(error.error.error);
+            // this.loader.presentToast(error.error.message);
+          });
+          // location.reload()
+        })
+      })).subscribe()
 
-    let payload = {
-      title: this.blogForm.value.title.toString,
-      details: this.blogForm.value.details.toString
-    }
-    console.log('payload : ', payload);
-    this.service.addBlogs(payload).subscribe(user => {
-      console.log('data saved')
-    }, error => {
-      console.log('Error : ', error.error)
-      // this.loader.presentToast(error.error.error);
-      // this.loader.presentToast(error.error.message);
-    });
+    // let payload = {
+    //   title: this.blogForm.value.title,
+    //   details: this.blogForm.value.details
+    // }
+  
   }
  
 }
